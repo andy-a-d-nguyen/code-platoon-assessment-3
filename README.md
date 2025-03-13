5d44156daa531e5014428cbeae0d3a36780e2a98
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2025-03-13 16:21:00 -0700

    Clean Up

--- README.md
+++ README.md
@@ -6,17 +6,36 @@ Your goal is to automate the deployment of a three-tier application stack throug
 We will provide a few examples to get you started.
 
 ## Instructions
+
 1. Fork and clone down this repo onto your local machine
 2. You can use either of the subdirectories in the templates folder to get you started, or you can create your own app from scratch
 
 ## Recommended Steps
+
 1. Ensure the core functionality of your app is working locally
 2. Containerize each part of your application
 3. Write workflows to:
    - Push your image(s)
    - Provision remote resources
    - Deploy and run your application remotely
-  
+
 You can find additional instructions and a scoring/requirements rubric in the [Assessment 3 Document](https://docs.google.com/document/d/1IWGg04gIUM2j9hj8cyiFhqTTRcr-NvwhIEeYEbRIIQo/edit?tab=t.kt3fj1iax9hz#heading=h.2mviilujhigk)
 
 The assessment is due on the 15th.  You can present earlier for bonus points.
+
+## Setting Up the Pipeline
+
+1. Clone the repo
+
+2. Create the following environment variables
+
+```env
+AWS_ACCESS_KEY_ID=<your_access_key_ID>
+AWS_SECRET_ACCESS_KEY=<your_secret_access_key>
+AWS_SSH_KEY=<your_aws_ssh_key>
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=password
+POSTGRES_DB=movie_hero
+BACKEND_PORT=3001
+FRONTEND_PORT=3000
+```
--- main.tf
+++ main.tf
@@ -10,12 +10,50 @@ terraform {
   }
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-04b4f1a9cf54c11d0"
-  instance_type = "t2.micro"
+resource "aws_security_group" "andy_assessment_3" {
+  name        = "andy_assessment_3"
+  description = "Allow inbound traffic on port 3000 and 22"
 
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Open to all IPs; restrict as needed for security
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Open to all IPs; restrict as needed for security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  key_name               = "andy-east1"
+  vpc_security_group_ids = [aws_security_group.andy_assessment_3.id]
   tags = {
     Name = "Andy Ubuntu"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Checking EC2 liveness..."
+      while ! nc -zv ${aws_instance.ubuntu.public_ip} 22; do
+        echo "Waiting for EC2 instance to respond on port 22..."
+        sleep 5
+      done
+      echo "EC2 instance is live!"
+    EOT
   }
 }
 
